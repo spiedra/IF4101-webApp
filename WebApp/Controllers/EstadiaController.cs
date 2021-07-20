68c69d0bf9f396fad787d01367c3c95159240655
spiedra <jcspiedra@gmail.com>
2021-07-20 10:44:21 -0600

    sql folder and estadiaRegisterView completed

---
+++ WebApp/Controllers/EstadiaController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class EstadiaController : Controller
+    {
+        [Route("Estadia/Registrar")]
+        public ActionResult EstadiaRegistrar()
+        {
+            return View();
+        }
+
+        // GET: EstadiaControllercs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EstadiaControllercs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EstadiaControllercs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EstadiaControllercs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EstadiaControllercs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EstadiaControllercs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EstadiaControllercs/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApp/Models/EstadiaViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class EstadiaViewModel
+    {
+        [Required(ErrorMessage = "Por favor ingrese el nombre de la estancia")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese el nombre de la provincia")]
+        public string Provincia { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese la dirección exacta")]
+        public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese el precio por noche")]
+        public long PrecionNoche { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese la capacidad")]
+        public int Capacidad { get; set; }
+
+        [Required(ErrorMessage = "Por favor suba alguna imagen")]
+        public string RutaImagen { get; set; }
+
+        [Required(ErrorMessage = "Por favor seleccione algun tipo de categoria")]
+        public string TipoCategoria { get; set; }
+
+        [Required(ErrorMessage = "Por favor ingrese alguna descipción")]
+        public int Descripcion { get; set; }
+    }
+}
